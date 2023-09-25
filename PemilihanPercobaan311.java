d1132fc289f1d8ed24ad7b541d5e1c28e60045d0
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-09-25 16:04:18 +0700

    PemilihanPercobaan311

---
+++ PemilihanPercobaan311.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class PemilihanPercobaan311 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukan Angka Pertama");
+        angka1 = input11.nextDouble();
+        System.out.print("Masukan Angka Kedua");
+        angka2 = input11.nextDouble();
+        System.out.print("Masukan Operator (+ - * /) :");
+        operator = input11.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+            hasil = angka1 + angka2;
+            System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+             case '-':
+            hasil = angka1 - angka2;
+            System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+             case '*':
+            hasil = angka1 * angka2;
+            System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+             case '/':
+            hasil = angka1 / angka2;
+            System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+        }
+
+
+
+    }
+    
+}
