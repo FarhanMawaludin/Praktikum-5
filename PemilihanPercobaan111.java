d43478c5f8f8b1ab13d185ff86d487f211e6ef2e
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-09-25 13:22:22 +0700

    tugas akuh

---
+++ PemilihanPercobaan111.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan111 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        
+
+        System.out.println("Masukan Angka");
+        int angka = input11.nextInt();
+
+        if (angka % 2 == 0)
+        System.out.println("Angka"+angka+"bilangan genap");
+        else 
+        System.out.println("Angka"+angka+"bilangan ganjil");
+
+
+
+    }
+}
